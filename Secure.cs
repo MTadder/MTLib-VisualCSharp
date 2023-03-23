d9f5c553c5c3a3fc00aa4732cd39fb26a63e91dd
MTadder <mtadder@protonmail.com>
2023-03-23 04:25:50 -0400

    Cleaning up Secure.cs

--- Secure.cs
+++ Secure.cs
@@ -4,50 +4,38 @@
     /// </summary>
     public static class Secure {
         /// <summary>
-        /// Represents a trustworthy <see cref="_keyset">keyset</see> holder and/or verifier
+        /// Represents a trustworthy <see cref="keyset">keyset</see> holder and/or verifier
         /// </summary>
         public class Authenticator {
-            protected internal List<string> _keyset = new();
-            protected internal Salt _salt = new();
+            private List<string> keyset = new();
+            private Salt salt = new();
             public Salt Salt {
-                get { return this._salt; }
-                set { this._salt=value; }
+                get { return this.salt; }
+                set { this.salt=value; }
             }
-            public void Clear() {
-                this._keyset.Clear();
-            }
-            public void Register(string key) {
-                this._keyset.Add(this.Hash(key));
-            }
+            public void Clear() => this.keyset.Clear();
+            public void Register(string key) => this.keyset.Add(this.Hash(key));
             public void Register(string[] keys) {
                 string[] hashedKeys = new string[keys.Length];
                 for (int i = 0; i<keys.Length; i++) {
                     hashedKeys[i]=this.Hash(keys[i]);
                 }
-                this._keyset.AddRange(hashedKeys);
+                this.keyset.AddRange(hashedKeys);
             }
-            public void Register(List<string> keys) {
-                this.Register(keys.ToArray());
-            }
-            public void Register(char key) {
-                this.Register(key.ToString());
-            }
-            public void Register(float key) {
-                this.Register(key.ToString());
-            }
-            public void Register(double key) {
-                this.Register(key.ToString());
-            }
+            public void Register(List<string> keys) => this.Register(keys.ToArray());
+            public void Register(char key) => Register(key.ToString());
+            public void Register(float key) => Register(key.ToString());
+            public void Register(double key) => Register(key.ToString());
             public bool IsRegistered(string query, bool doHash = true) {
                 string hashedQuery = doHash ? this.Hash(query) : query;
-                foreach (string key in this._keyset) {
+                foreach (string key in this.keyset) {
                     if (key.Equals(hashedQuery)) { return true; }
                 }
                 return false;
             }
             public void Persist(DictionaryFile<string, string> into) {
                 int idx = 0;
-                foreach (string key in this._keyset) {
+                foreach (string key in this.keyset) {
                     into[idx.ToString()]=key;
                 }
             }
@@ -55,7 +43,7 @@
             public Authenticator(DictionaryFile<string, string> df) {
                 int idx = 0;
                 while (df.Memory.ContainsKey(idx.ToString())) {
-                    this._keyset.Insert(idx, df[idx.ToString()]);
+                    this.keyset.Insert(idx, df[idx.ToString()]);
                 }
             }
             public string Hash(string data) {
@@ -63,7 +51,7 @@
             }
         }
         public class Salt {
-            protected internal char[] _salt;
+            private char[] data;
             protected internal static char[] GenerateSegments(Int32 amount) {
                 static char[] GenerateSegment() {
                     return Guid.NewGuid().ToString().Replace("-", "").ToCharArray();
@@ -77,19 +65,13 @@
                 }
                 return Segments.ToArray();
             }
-            public Salt(int segments = 13) {
-                this._salt=GenerateSegments(segments);
-            }
-            public Salt(Salt target) {
-                this._salt=target._salt;
-            }
-            public Salt(string salt) {
-                this._salt=salt.ToCharArray();
-            }
+            public Salt(int segments = 13) => this.data=GenerateSegments(segments);
+            public Salt(Salt target) => this.data=target.data;
+            public Salt(string salt) => this.data=salt.ToCharArray();
             public string Hash(string data) {
                 string result = "";
                 for (int dataCharIdx = 0; dataCharIdx<data.Length; dataCharIdx++) {
-                    char saltChar = this._salt[dataCharIdx%this._salt.Length];
+                    char saltChar = this.data[dataCharIdx%this.data.Length];
                     char dataChar = data[dataCharIdx];
                     result+=char.ConvertFromUtf32(((int) saltChar%(int) dataChar)+0xE)??"+";
                 }
